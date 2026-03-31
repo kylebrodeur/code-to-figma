e58453b3d2b2e45b13e6216a36b68433865e0997
kylebrodeur <kyle@brodeur.me>
2026-03-31 12:55:56 -0500

    wip

---
+++ bss-app/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/094p_next_dist_compiled_0o4v89u._.js",
+  "static/chunks/094p_next_dist_shared_lib_0rwn3y1._.js",
+  "static/chunks/094p_next_dist_client_0gduyha._.js",
+  "static/chunks/094p_next_dist_11qy~xr._.js",
+  "static/chunks/0wtj_next_dist_pages__error_007axxo.js",
+  "static/chunks/[next]_entry_page-loader_ts_0ivt5gt._.js",
+  "static/chunks/0p2h_react-dom_0vsf22y._.js",
+  "static/chunks/node_modules__pnpm_13qx_gy._.js",
+  "static/chunks/[root-of-the-server]__0-a8rlf._.js",
+  "static/chunks/[project]_bss-app_pages__error_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_bss-app_pages__error_0dl2on6._.js"
+])
